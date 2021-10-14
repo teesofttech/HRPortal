71f0cb934aad353089f0b272426f649bedeb4940
Babatunde Esanju <tundeesanju@gmail.com>
2021-10-14 15:57:38 +0100

    commit

--- HRPortal.Web/Controllers/DashboardController.cs
+++ HRPortal.Web/Controllers/DashboardController.cs
@@ -153,8 +153,58 @@ namespace HRPortal.Web.Controllers
 
         public async Task<IActionResult> Apply(int id)
         {
-            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            //var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Apply(IFormFile formFile)
+        {
+            string logoFileName = null;
+            string pathfile = null;
+
+            string cacFileName = null;
+            string pathfile1 = null;
+            try
+            {
+                if (formFile == null)
+                {
+                    ViewData["error"] = "No image for either company logo and company document";
+                    return RedirectToAction("UploadResume", "Dashboard");
+                }
+                else
+                {
+                    string _logo = Path.Combine(_hostingEnvironment.WebRootPath, "cv");
+                    logoFileName = Guid.NewGuid().ToString() + "_" + formFile.FileName;
+                    pathfile = Path.Combine(_logo, logoFileName);
+                    FileStream fs = new FileStream(pathfile, FileMode.Create);
+                    formFile.CopyTo(fs);
+
+                    fs.Close();
+
+
+                    var extension = Path.GetExtension(formFile.FileName);
+                    const string mimeType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+                    if (string.Equals(extension, ".docx") && formFile.ContentType.Equals(mimeType))
+                    {
+                        var dictionaryPath = Path.Combine(_hostingEnvironment.WebRootPath, "KeywordDictionary.xml");
+                        var modelData = Engine.Parse(pathfile, dictionaryPath);
+                        return View(JsonConvert.DeserializeObject<DocumentViewModel>(modelData));
+                    }
+                    else
+                    {
+                        //ViewData["success"] = "Your account has been created successfully";
+                        return RedirectToAction("UploadResume", "Dashboard");
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                ViewData["error"] = "Errors occurred while creating vendor";
+                return RedirectToAction("UploadResume", "UploadResume");
+            }
         }
     }
 }
